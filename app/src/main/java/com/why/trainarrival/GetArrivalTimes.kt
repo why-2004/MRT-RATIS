dea6568c1458d60b7e0bfeb95913203e8b6049a3
why-2004 <h1710149@nushigh.edu.sg>
2020-08-21 20:58:12 +0800

    0.5-alpha:  commented out train departure detection since it's not really useful rn  added 4 platform so it's actually useful  supported the 3 most useful stations  "API" basically finalized and working
    Signed-off-by: why-2004 <h1710149@nushigh.edu.sg>

--- app/src/main/java/com/why/trainarrival/GetArrivalTimes.kt
+++ app/src/main/java/com/why/trainarrival/GetArrivalTimes.kt
@@ -6,12 +6,12 @@ import okhttp3.Request
 import org.jsoup.Jsoup
 import org.jsoup.select.Elements
 
-public class GetArrivalTimes(private val stn:String, private val viewState:String){
+public class GetArrivalTimes(public var stn:String, private val viewState:String){
     public var trains=ArrayList<Pair<Int,String>>()
     private var times= Elements()
     private var dests=Elements()
     private val client = OkHttpClient()
-    private val formBody:FormBody = FormBody.Builder()
+    private var formBody:FormBody = FormBody.Builder()
         .add("ScriptManager1", "UP1|ddlStation")
         .add("__LASTFOCUS","")
         .add("__EVENTTARGET","ddlStation")
@@ -23,13 +23,51 @@ public class GetArrivalTimes(private val stn:String, private val viewState:Strin
         .add("ddlStation",this.stn)
         .build()
 
-    private val request = Request.Builder()
+    fun refreshFormBody(stn:String,x:MainActivity){
+        this.formBody = FormBody.Builder()
+        .add("ScriptManager1", "UP1|ddlStation")
+            .add("__LASTFOCUS","")
+            .add("__EVENTTARGET","ddlStation")
+            .add("__EVENTARGUMENT","")
+            .add("__VIEWSTATE",this.viewState)
+            .add("__VIEWSTATEGENERATOR","CA0B0334")
+            .add("stnCode","")
+            .add("stnName","")
+            .add("ddlStation",stn)
+            .build()
+
+        this.request = Request.Builder()
+            .url("http://trainarrivalweb.smrt.com.sg/default.aspx")
+            .addHeader("Host","""trainarrivalweb.smrt.com.sg""")
+            .addHeader("Accept-Language","""en-US,en;q=0.5""")
+            .addHeader("Accept","""*/*""")
+            .addHeader("Cache-Control", "no-cache")
+            .addHeader("Content-Type", """application/x-www-form-urlencoded""")
+            .addHeader("Content-Length","7831")
+            .addHeader("Origin","""http://trainarrivalweb.smrt.com.sg""")
+            .addHeader("Connection","""keep-alive""")
+            .addHeader("Referer","""http://trainarrivalweb.smrt.com.sg/default.aspx""")
+            .addHeader("cookie","""ASP.NET_SessionId=umv0ktalceechc3ejkykytvd""")
+            .post(formBody)
+            .build()
+
+        this.stn=stn
+        println(stn)
+        this.trains=ArrayList()
+        
+        getTimes()
+        x.updateText()
+
+    }
+
+    private var request = Request.Builder()
         .url("http://trainarrivalweb.smrt.com.sg/default.aspx")
         .addHeader("Host","""trainarrivalweb.smrt.com.sg""")
         .addHeader("Accept-Language","""en-US,en;q=0.5""")
         .addHeader("Accept","""*/*""")
         .addHeader("Cache-Control", "no-cache")
         .addHeader("Content-Type", """application/x-www-form-urlencoded""")
+        .addHeader("Content-Length","7831")
         .addHeader("Origin","""http://trainarrivalweb.smrt.com.sg""")
         .addHeader("Connection","""keep-alive""")
         .addHeader("Referer","""http://trainarrivalweb.smrt.com.sg/default.aspx""")
@@ -43,12 +81,13 @@ public class GetArrivalTimes(private val stn:String, private val viewState:Strin
             client.newCall(request).execute().use { response ->
                 if (!response.isSuccessful) {
                     println("failure")
+
                 } else {
                     val s = response.body!!.string()
                     val doc = Jsoup.parse(s)
                     times = doc.getElementsContainingOwnText("""min(s)""")
                     dests =
-                        doc.getElementsMatchingOwnText("^((Pasir Ris)|(Joo Koon)|(Tuas Link)|(Do Not Board))$")
+                        doc.getElementsMatchingOwnText("^((Woodlands North)|(Woodlands South)|(Jurong East)|(Marina South Pier)|(Marina Bay)|(HarbourFront)|(Dhoby Ghaut)|(Boon Lay)|(Tanah Merah)|(Changi Airport)|(Pasir Ris)|(Joo Koon)|(Tuas Link)|(Do Not Board))$")
                     if(this.times.size==0){
                         println("what???")
                         println(s)
@@ -75,6 +114,7 @@ public class GetArrivalTimes(private val stn:String, private val viewState:Strin
             }
         }catch(e:Exception){
             println("failure")
+            println(e)
         }
     }
 
